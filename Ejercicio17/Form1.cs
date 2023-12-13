21d5528bf5dbff7d9566f9f038f442cf53348d42
ÓSCAR SALA LÓPEZ <oscsallop2@alu.edu.gva.es>
2023-12-13 16:37:59 +0100

    Codigo corregido

--- Ejercicio17/Form1.cs
+++ Ejercicio17/Form1.cs
@@ -19,16 +19,16 @@ namespace ExamenED1EV2324
 
         // Función que realiza una división entera mediante restas sucesivas.
         // Contamos cuantas veces "cabe" el dividendo en el divisor.
-        int divisionRestas(int dividendo, int divisor)
+        int divisionRestas(int OSL, int DAW1)
         {
             // Declaramos una variable para almacenar el resultado.
             int cont = 0;
-
+            
             // Restamos el divisor al dividendo
-            //
-            while (dividendo > divisor)
+            
+            while (OSL > DAW1)
             {
-                    divisor -= dividendo;
+                    OSL -= DAW1;
                     cont++;
             }
 
@@ -42,30 +42,30 @@ namespace ExamenED1EV2324
             try
             {
                 // Declaramos las variables necesarias.
-                int dividendo, divisor, resultado;
+                int OSL, DAW1, resultado;
 
                 // Leemos los valores del cuadro de texto.
-                dividendo = int.Parse(txtDividendo.Text);
-                divisor = int.Parse(txtDivisor.Text);
+                OSL = int.Parse(txtDividendo.Text);
+                DAW1 = int.Parse(txtDivisor.Text);
 
                 // Comprobamos que el divisor no sea cero.
-                if (divisor == 0)
+                if (DAW1 == 0)
                 {
                     // Lanzamos una excepción.
                     throw new Exception("El divisor no puede ser cero.");
                 }
                 // Comprobamos que el divisor no sea mayor que el dividendo.
-                if (dividendo <= divisor)
+                if (OSL < DAW1)
                 {
                     // Lanzamos una excepción.
                     throw new Exception("El divisor ha de ser menor que el dividendo.");
                 }
 
                 // Llamamos a la función para realizar la división.
-                resultado = divisionRestas(dividendo, divisor);
+                resultado = divisionRestas(OSL, DAW1);
 
                 // Mostramos el resultado.
-                MessageBox.Show("El resultado de dividir " + dividendo + " entre " + divisor + " es : " + resultado);
+                MessageBox.Show("El resultado de dividir " + OSL + " entre " + DAW1 + " es : " + resultado);
             }
             catch (FormatException ex)
             {
