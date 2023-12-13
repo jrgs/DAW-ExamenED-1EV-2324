049b824da55f386a147a1163a2be0e66aefc7a64
ASENSIO EMILIO REBOLLO GÓMEZ <aserebgom@alu.edu.gva.es>
2023-12-13 16:51:30 +0100

    AERG2324.Corregimos los tres errores logicos

--- Ejercicio17/Form1.cs
+++ Ejercicio17/Form1.cs
@@ -19,16 +19,18 @@ namespace ExamenED1EV2324
 
         // Función que realiza una división entera mediante restas sucesivas.
         // Contamos cuantas veces "cabe" el dividendo en el divisor.
-        int divisionRestas(int dividendo, int divisor)
+        int divisionRestas(int AERG2324dividendo, int AERG2324divisor)
         {
             // Declaramos una variable para almacenar el resultado.
-            int cont = 0;
-
+            //AERG2324.SEGUNDO ERROR.MODIFICAMOS EL VALOR INICIAL DE CONT 
+            //int cont = 0;
+            int cont = 1;
             // Restamos el divisor al dividendo
             //
-            while (dividendo > divisor)
-            {
-                    divisor -= dividendo;
+            while (AERG2324dividendo > AERG2324divisor)
+            {//! AERG2324.PRIMER ERROR.SE HA DE CAMBIARLAS VARIABLES DE ORDEN
+                AERG2324dividendo -= AERG2324divisor;
+                    //AERG2324divisor -= AERG2324dividendo;
                     cont++;
             }
 
@@ -42,30 +44,32 @@ namespace ExamenED1EV2324
             try
             {
                 // Declaramos las variables necesarias.
-                int dividendo, divisor, resultado;
+                int AERG2324dividendo, AERG2324divisor, resultado;
 
                 // Leemos los valores del cuadro de texto.
-                dividendo = int.Parse(txtDividendo.Text);
-                divisor = int.Parse(txtDivisor.Text);
+                AERG2324dividendo = int.Parse(txtDividendo.Text);
+                AERG2324divisor = int.Parse(txtDivisor.Text);
 
                 // Comprobamos que el divisor no sea cero.
-                if (divisor == 0)
+                if (AERG2324divisor == 0)
                 {
                     // Lanzamos una excepción.
                     throw new Exception("El divisor no puede ser cero.");
                 }
                 // Comprobamos que el divisor no sea mayor que el dividendo.
-                if (dividendo <= divisor)
+                //AERG2324.TERCER ERROR.SE HA DE QUITAR EL SIGNO = PARA QUE NO SALTE A LA EXCEPCION EN CASO DE QUE DIVIDENDO Y DIVISOR SEAN IGUALES
+               // if (AERG2324dividendo <= AERG2324divisor)
+               if(AERG2324dividendo < AERG2324divisor)
                 {
                     // Lanzamos una excepción.
                     throw new Exception("El divisor ha de ser menor que el dividendo.");
                 }
 
                 // Llamamos a la función para realizar la división.
-                resultado = divisionRestas(dividendo, divisor);
+                resultado = divisionRestas(AERG2324dividendo, AERG2324divisor);
 
                 // Mostramos el resultado.
-                MessageBox.Show("El resultado de dividir " + dividendo + " entre " + divisor + " es : " + resultado);
+                MessageBox.Show("El resultado de dividir " + AERG2324dividendo + " entre " + AERG2324divisor + " es : " + resultado);
             }
             catch (FormatException ex)
             {
